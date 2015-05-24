aafc5d6e2b8d2a327ab489009a7fa5d16918a9cd
Michael Kutý <6du1ro.n@gmail.com>
2015-05-24 16:50:38 +0200

    preview region

--- leonardo_module_blog/models.py
+++ leonardo_module_blog/models.py
@@ -6,21 +6,25 @@ from leonardo.module import media, web
 Entry.register_extensions('feincms.module.extensions.datepublisher',
                           'feincms.module.extensions.translations',
                           )
+
+REGIONS = ('preview', 'main',)
+
 Entry.register_regions(
+    ('preview', _('Preview content area')),
     ('main', _('Main content area')),
 )
 
 Entry.create_content_type(
-    web.widget.HtmlTextWidget, regions=('main',), optgroup=_('Text'))
+    web.widget.HtmlTextWidget, regions=REGIONS, optgroup=_('Text'))
 Entry.create_content_type(
-    web.widget.MarkupTextWidget, regions=('main',), optgroup=_('Text'))
+    web.widget.MarkupTextWidget, regions=REGIONS, optgroup=_('Text'))
 
 for widget in media.default.widgets:
-    Entry.create_content_type(widget, regions=('main',), optgroup=_('Media'))
+    Entry.create_content_type(widget, regions=REGIONS, optgroup=_('Media'))
 
 try:
     from leonardo_oembed.widget import OembedWidget
     Entry.create_content_type(OembedWidget,
-                              regions=('main',), optgroup=_('External content'))
+                              regions=REGIONS, optgroup=_('External content'))
 except:
     pass
