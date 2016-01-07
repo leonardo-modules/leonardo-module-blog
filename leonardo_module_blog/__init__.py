5b2c3a6622895108f691418feef62e714722d01f
Michael Kutý <6du1ro.n@gmail.com>
2016-01-07 15:04:38 +0100

    Just cleanup imports and change to new leonardo paths.

--- leonardo_module_blog/__init__.py
+++ leonardo_module_blog/__init__.py
@@ -2,9 +2,6 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
-from .widget import *
-
-
 default_app_config = 'leonardo_module_blog.BlogConfig'
 
 
@@ -23,8 +20,8 @@ class Default(object):
     @property
     def widgets(self):
         return [
-            BlogCategoriesWidget,
-            RecentBlogPostsWidget,
+            'leonardo_module_blog.models.BlogCategoriesWidget',
+            'leonardo_module_blog.models.RecentBlogPostsWidget',
         ]
 
     @property
--- leonardo_module_blog/models.py
+++ leonardo_module_blog/models.py
@@ -1,7 +1,12 @@
 
+import six
 from django.utils.translation import ugettext_lazy as _
 from elephantblog.models import Entry
+from importlib import import_module
 from leonardo.module import media, web
+
+from .widget.blogcategories.models import BlogCategoriesWidget
+from .widget.recentblogposts.models import RecentBlogPostsWidget
 
 Entry.register_extensions('leonardo.extensions.datepublisher',
                           'leonardo.extensions.translations',
@@ -14,16 +19,24 @@ Entry.register_regions(
     ('main', _('Main content area')),
 )
 
+
+def get_class_from_string(widget):
+    mod = '.'.join(widget.split('.')[0:-1])
+    cls_name = widget.split('.')[-1]
+    return getattr(import_module(mod), cls_name)
+
 Entry.create_content_type(
-    web.widget.HtmlTextWidget, regions=REGIONS, optgroup=_('Text'))
+    web.models.HtmlTextWidget, regions=REGIONS, optgroup=_('Text'))
 Entry.create_content_type(
-    web.widget.MarkupTextWidget, regions=REGIONS, optgroup=_('Text'))
+    web.models.MarkupTextWidget, regions=REGIONS, optgroup=_('Text'))
 
 for widget in media.default.widgets:
+    if isinstance(widget, six.string_types):
+        widget = get_class_from_string(widget)
     Entry.create_content_type(widget, regions=REGIONS, optgroup=_('Media'))
 
 try:
-    from leonardo_oembed.widget import OembedWidget
+    from leonardo_oembed.models import OembedWidget
     Entry.create_content_type(OembedWidget,
                               regions=REGIONS, optgroup=_('External content'))
 except:
